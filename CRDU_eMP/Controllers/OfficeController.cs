16a1b2f1dc9423ab19dabfc7ba27b422ed7f8bfc
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-08-05 02:05:58 +0300

    Added employee and office to nav bar, implemented office CRUD operations, and styled the website using Bootstrap.

---
+++ CRDU_eMP/Controllers/OfficeController.cs
@@ -0,0 +1,144 @@
+﻿using CRDU_eMP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRDU_eMP.Controllers
+{
+    public class OfficeController : Controller
+    {
+        private readonly EFContext _context;
+
+        public OfficeController(EFContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            List<Office> offices = _context.Offices.ToList();
+            return View(offices);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = _context.Offices.FirstOrDefault(o => o.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // GET: Office/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Office/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Office office)
+        {
+            try
+            {
+                _context.Add(office);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return Content($"Something went wrong\n {ex}");
+            }
+
+            return View(office);
+        }
+
+        // GET: Office/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = _context.Offices.FirstOrDefault(o => o.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Office office)
+        {
+            Office oldOffice = _context.Offices.SingleOrDefault(o => o.Id == office.Id);
+
+            if (id != office.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                oldOffice.Name = office.Name;
+                oldOffice.Location = office.Location;
+
+                _context.Update(oldOffice);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return Content($"Something went wrong\n {ex}");
+            }
+
+            return View(office);
+        }
+
+        // GET: Office/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = _context.Offices.FirstOrDefault(o => o.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // POST: Office/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var office = _context.Offices.FirstOrDefault(o => o.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            _context.Offices.Remove(office);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
