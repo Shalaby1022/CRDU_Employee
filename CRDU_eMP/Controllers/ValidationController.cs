fbd78ff9e31eef4d54b878bb6382c7d0d6cb837e
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-08-06 17:08:31 +0300

    Add Employee Validation Controller - AddEmployee ViewModel - Use Validation in Add Form

---
+++ CRDU_eMP/Controllers/ValidationController.cs
@@ -0,0 +1,60 @@
+﻿using CRDU_eMP.Models;
+using CRDU_eMP.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRDU_eMP.Controllers
+{
+    public class ValidationController : Controller
+    {
+        EFContext _context;
+        public ValidationController(EFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult add()
+        {
+            AddEmpView addEmpView = new AddEmpView() { Offices = new SelectList(_context.Offices.ToList(), "Id", "Name") };
+
+            return View(addEmpView);
+        }
+        [HttpPost]
+        public IActionResult add( AddEmpView addEmpView)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = new Employee();
+                employee.Id = addEmpView.Id;
+                employee.Name = addEmpView.Name;
+                employee.Salary = addEmpView.Salary;
+                employee.Age = addEmpView.Age;
+                employee.Email = addEmpView.Email;
+                employee.Password = addEmpView.Password;
+                employee.OfficeId = addEmpView.Off_Id;
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                addEmpView.Offices = new SelectList(_context.Offices.ToList(), "Id", "Name");
+                return View(addEmpView);
+            }
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
---
+++ CRDU_eMP/ViewModel/AddEmpView.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRDU_eMP.ViewModel
+{
+    public class AddEmpView
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be from 3 to 30 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(20, 60, ErrorMessage = "20 to 60 ")]
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email")]
+        [RegularExpression(".*@[a-z0-9.-]*")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password, ErrorMessage = "Password must have" +
+            "must contains one digit from 0-9\r\n  " +
+            "must contains one lowercase characters\r\n  " +
+            "must contains at least one special character\r\n " +
+            "match anything with previous condition checking\r\n" +
+            "length at least 8 characters and maximum of 20")]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{6,20})")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Does not match")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "Office")]
+        public int Off_Id { get; set; }
+        [ValidateNever]
+        public SelectList Offices { get; set; }
+
+
+    }
+}
+
